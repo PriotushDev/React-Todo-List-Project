dfbe9ea93370a28a5fb422f39f9e0175bc91e87f
Priyotosh <ai.priyobabu@gmail.com>
2026-02-05 21:14:11 +0600

    pages and routes added

---
+++ src/pages/PostDetails.jsx
@@ -0,0 +1,8 @@
+export default function Posts()
+{
+    return(
+        <div>
+            <h1>This is Post Detail Page</h1>
+        </div>
+    )
+}
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,8 @@
+export default function Posts()
+{
+    return(
+        <div>
+            <h1>This is Post Page</h1>
+        </div>
+    )
+}
