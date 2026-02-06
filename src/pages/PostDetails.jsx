c606be00da9f1930c5196b7b64370e1c72b9ddb8
Priyotosh <ai.priyobabu@gmail.com>
2026-02-06 18:43:52 +0600

    Show Post Detail view on PostDetail page

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,8 +1,53 @@
-export default function Posts()
-{
-    return(
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import Loader from "../components/Loader";
+
+export default function PostDetails() {
+    const { id } = useParams();
+
+    const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    // ⭐ favorite state (localStorage)
+    const [favorites, setFavorites] = useState(
+        JSON.parse(localStorage.getItem("favPosts")) || []
+    );
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            setLoading(true);   // lifecycle start
+            setError(null);     // reset previous error
+
+            try {
+                const response = await axios.get(
+                    `https://jsonplaceholder.typicode.com/posts/${id}`
+                );
+                setPost(response.data);
+            } catch (err) {
+                setError("Failed to load post");
+            } finally {
+                setLoading(false); // lifecycle end
+            }
+        };
+
+        fetchPost();
+    }, [id]);
+
+
+    if (loading) return <Loader />;
+
+    if (error) return <p>Error: {error}</p>;
+
+    return (
         <div>
-            <h1>This is Post Detail Page</h1>
+            <Link to="/posts">⬅ Back to Posts</Link>
+
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+
+
         </div>
-    )
+    );
 }
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -4,63 +4,64 @@ import Loader from "../components/Loader";
 import { Link } from "react-router-dom";
 
 export default function Posts() {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [search, setSearch] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            setLoading(true);
-            setError(false);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      setError(false);
 
-            try {
-                const response = await axios.get(
-                    "https://jsonplaceholder.typicode.com/posts?_limit=10",
-                );
-                setPosts(response.data);
-            } catch (error) {
-                setError("Failed to load post");
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchPosts();
-    }, []);
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10",
+        );
+        setPosts(response.data);
+      } catch (error) {
+        setError("Failed to load post");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
 
-    const filteredPosts = posts.filter((post) =>
-        post.title.toLowerCase().includes(search.toLocaleLowerCase()),
-    );
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(search.toLocaleLowerCase()),
+  );
 
-    if (loading) {
-        return <Loader />;
-    }
+  if (loading) {
+    return <Loader />;
+  }
 
-    if (error) {
-        return <p>Error: {error}</p>;
-    }
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
-    return (
-        <div>
-            <h1>Posts</h1>
+  return (
+    <div>
+      <h1>Posts</h1>
 
-            {/* search */}
-            <input
-                type="text"
-                placeholder="search post..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-            />
-            <ul>
-                {filteredPosts.map((post) => (
-                    <li key={post.id}>
-                        <h3>{post.title}</h3>
-                        <p>{post.body.substring(0, 80)}...</p>
+      {/* search */}
+      <input
+        type="text"
+        placeholder="search post..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <ul>
+        {filteredPosts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body.substring(0, 80)}...</p>
 
-                        <Link to = {`/posts/${post.id}`}>View Detail</Link>
-                    </li>
-                ))};
-            </ul>
-        </div>
-    );
+            <Link to={`/posts/${post.id}`}>View Detail</Link>
+          </li>
+        ))}
+        ;
+      </ul>
+    </div>
+  );
 }
